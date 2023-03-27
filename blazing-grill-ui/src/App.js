4379cf2aa61ad3dd4fad3c557b0170bd0ecc516c
kaleem99 <kaleem1999@outlook.com>
2023-03-27 14:27:30 +0200

    maintenance in progress

--- blazing-grill-ui/src/App.js
+++ blazing-grill-ui/src/App.js
@@ -19,112 +19,118 @@ const defaultOptions = {
   },
 };
 const sections = ["Home", "Add Menu Item", "image", "Orders", "Account"];
+// function App() {
+//   const [state, setState] = useState("Home");
+//   const [isLoggedIn, setIsLoggedIn] = useState(undefined);
+//   const [storeDetails, setStoreDetails] = useState([]);
+//   // const [signInRegister, setSignInRegister] = useState("Login");
+//   const [storeStatus, setstoreStatus] = useState(false);
+//   const [currentStore, setCurrentStore] = useState("");
+//   const [email, setEmail] = useState("");
+
+//   useEffect(() => {
+//     onAuthStateChanged(auth, (user) => {
+//       getDocs(collection(db, "BlazingStores")).then((querySnapshot) => {
+//         const newData = querySnapshot.docs.map((doc) => ({
+//           ...doc.data(),
+//           id: doc.id,
+//         }));
+//         setStoreDetails(newData);
+//       });
+//       if (user && user.emailVerified) {
+//         const uid = user.uid;
+//         setIsLoggedIn(true);
+//         setEmail(user.email);
+//         const initialStore = storeDetails.map((stores) =>
+//           stores.email === user.email ? setCurrentStore(stores) : stores
+//         );
+//       } else {
+//         setIsLoggedIn(false);
+//         console.log("user is logged out", isLoggedIn);
+//         setState("Login");
+//       }
+//     });
+//     const data = getDocs(collection(db, "BlazingStores")).then(
+//       (querySnapshot) => {
+//         const newData = querySnapshot.docs.map((doc) => ({
+//           ...doc.data(),
+//           id: doc.id,
+//         }));
+//         let Name = Object.keys(newData[0])[0];
+//         setstoreStatus(newData[0][Name].storeStatus);
+//       }
+//     );
+//   }, [storeDetails, storeStatus]);
+//   const store = storeDetails.map((stores, i) => {
+//     if (stores[Object.keys(storeDetails[i])[0]].adminUsername === email) {
+//       return stores;
+//     }
+//   });
+//   const loginRegister = () => {
+//     switch (state) {
+//       case "Register":
+//         return <Register setState={setState} />;
+//       default:
+//         return (
+//           <Login
+//             setState={setState}
+//             // store={store}
+//             storeDetails={storeDetails}
+//             setStoreDetails={setStoreDetails}
+//           />
+//         );
+//     }
+//   };
+//   if (isLoggedIn === undefined) {
+//     return (
+//       <div className="App">
+//         <Lottie options={defaultOptions} height={400} width={400} />;
+//       </div>
+//     );
+//   } else {
+//     return (
+//       <div className="App">
+//         {!isLoggedIn ? (
+//           <>
+//             {/* <Lottie options={defaultOptions} height={400} width={400} /> */}
+//             <img
+//               className="BlazingImage"
+//               alt=""
+//               src="https://www.theblazinggrill.co.za/wp-content/uploads/2021/07/TBG_Final_TransWhite.png"
+//             ></img>
+//             {loginRegister()}
+//           </>
+//         ) : (
+//           <>
+//             <NavMenu
+//               sections={sections}
+//               setState={setState}
+//               state={state}
+//               isLoggedIn={isLoggedIn}
+//             />
+//             <div className="sections">
+//               <Sections
+//                 state={state}
+//                 setState={setState}
+//                 isLoggedIn={isLoggedIn}
+//                 setStoreDetails={setStoreDetails}
+//                 storeDetails={storeDetails}
+//                 store={store}
+//                 storeStatus={storeStatus}
+//                 setstoreStatus={setstoreStatus}
+//               />
+//             </div>
+//           </>
+//         )}
+//       </div>
+//     );
+//   }
+// }
 function App() {
-  const [state, setState] = useState("Home");
-  const [isLoggedIn, setIsLoggedIn] = useState(undefined);
-  const [storeDetails, setStoreDetails] = useState([]);
-  // const [signInRegister, setSignInRegister] = useState("Login");
-  const [storeStatus, setstoreStatus] = useState(false);
-  const [currentStore, setCurrentStore] = useState("");
-  const [email, setEmail] = useState("");
-
-  useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      getDocs(collection(db, "BlazingStores")).then((querySnapshot) => {
-        const newData = querySnapshot.docs.map((doc) => ({
-          ...doc.data(),
-          id: doc.id,
-        }));
-        setStoreDetails(newData);
-      });
-      if (user && user.emailVerified) {
-        const uid = user.uid;
-        setIsLoggedIn(true);
-        setEmail(user.email);
-        const initialStore = storeDetails.map((stores) =>
-          stores.email === user.email ? setCurrentStore(stores) : stores
-        );
-      } else {
-        setIsLoggedIn(false);
-        console.log("user is logged out", isLoggedIn);
-        setState("Login");
-      }
-    });
-    const data = getDocs(collection(db, "BlazingStores")).then(
-      (querySnapshot) => {
-        const newData = querySnapshot.docs.map((doc) => ({
-          ...doc.data(),
-          id: doc.id,
-        }));
-        let Name = Object.keys(newData[0])[0];
-        setstoreStatus(newData[0][Name].storeStatus);
-      }
-    );
-  }, []);
-  const store = storeDetails.map((stores, i) => {
-    if (stores[Object.keys(storeDetails[i])[0]].adminUsername === email) {
-      return stores;
-    }
-  });
-  const loginRegister = () => {
-    switch (state) {
-      case "Register":
-        return <Register setState={setState} />;
-      default:
-        return (
-          <Login
-            setState={setState}
-            // store={store}
-            storeDetails={storeDetails}
-            setStoreDetails={setStoreDetails}
-          />
-        );
-    }
-  };
-  if (isLoggedIn === undefined) {
-    return (
-      <div className="App">
-        <Lottie options={defaultOptions} height={400} width={400} />;
-      </div>
-    );
-  } else {
-    return (
-      <div className="App">
-        {!isLoggedIn ? (
-          <>
-            {/* <Lottie options={defaultOptions} height={400} width={400} /> */}
-            <img
-              className="BlazingImage"
-              alt=""
-              src="https://www.theblazinggrill.co.za/wp-content/uploads/2021/07/TBG_Final_TransWhite.png"
-            ></img>
-            {loginRegister()}
-          </>
-        ) : (
-          <>
-            <NavMenu
-              sections={sections}
-              setState={setState}
-              state={state}
-              isLoggedIn={isLoggedIn}
-            />
-            <div className="sections">
-              <Sections
-                state={state}
-                setState={setState}
-                isLoggedIn={isLoggedIn}
-                setStoreDetails={setStoreDetails}
-                storeDetails={storeDetails}
-                store={store}
-                storeStatus={storeStatus}
-                setstoreStatus={setstoreStatus}
-              />
-            </div>
-          </>
-        )}
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1>Site is currently undergoing maintenance</h1>
+    </div>
+  );
 }
-
 export default App;
